c2d3272be53d437dfccb1444e5539ebd03741987
bed-nsh <bed.nsh.web@gmail.com>
2019-08-22 11:39:36 +0900

    처음 커밋

---
+++ src/main/java/com/sbs/cuni/cunicuniApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.cuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cunicuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cunicuniApplication.class, args);
+	}
+
+}
